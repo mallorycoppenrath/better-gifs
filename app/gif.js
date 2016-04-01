0e86dfd8b25a4794306dcbae8515b038e700d9b3
smernick <smernick3@gmail.com>
2016-04-01 11:06:07 -0400

    finished tag and gif model

---
+++ app/gif.js
@@ -0,0 +1,25 @@
+app.gif = {
+  all: [],
+  new: (function() {
+    var counter = 0
+    var gif = function Gif(url, tag) {
+      this.url = url
+      this.tag = tag
+      var that = this;
+      function initialize() {
+        counter++
+        that.id = counter;
+        app.gif.all.push(that);
+      }
+      initialize();
+    }
+    return gif;
+  }()),
+  findBy: function findBy(attrHash) {
+    var key = Object.keys(attrHash)[0];
+    var value = attrHash[key];
+    return $.grep(app.gif.all, function(gif){
+      return gif[key] == value;
+    });
+    },
+  }
---
+++ app/tag.js
@@ -0,0 +1,29 @@
+app.tag = {
+  all: [],
+  new: (function() {
+    var counter = 0
+    var tag = function Tag(description) {
+      this.description = description;
+      var that = this;
+      this.gifs = function(){
+        return app.gif.findBy({tag: this})
+      }
+
+
+      function initialize() {
+        counter++
+        that.id = counter;
+        app.tag.all.push(that);
+      }
+      initialize();
+    }
+    return tag;
+  }()),
+  findBy: function findBy(attrHash) {
+    var key = Object.keys(attrHash)[0];
+    var value = attrHash[key];
+    return $.grep(app.tag.all, function(tag){
+      return tag[key] == value;
+    });
+  },
+}
